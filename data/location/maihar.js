d4be7de4c600b9ee9b16e5ebad4f6963f758a7db
tathyadoot-png <tathyadoot@gmail.com>
2026-07-07 22:46:20 +0530

    fixed

--- data/location/maihar.js
+++ data/location/maihar.js
@@ -1,31 +1,21 @@
 const maihar = {
   district: "Maihar",
-
   tehsils: {
-    Maihar: {
+    "Maihar": {
       vidhansabha: "Maihar",
       janpad: "Maihar",
-      policeStations: [
-        "Maihar",
-      ],
+      policeStations: ["Maihar", "Amdara", "Badera"] // Added rural/industrial Thanas
     },
-
-    Amarpatan: {
+    "Amarpatan": {
       vidhansabha: "Amarpatan",
       janpad: "Amarpatan",
-      policeStations: [
-        "Amarpatan",
-      ],
+      policeStations: ["Amarpatan", "Nadan Dehat", "Tala"] // Added boundary Thanas
     },
-
-    Ramnagar: {
+    "Ramnagar": {
       vidhansabha: "Amarpatan",
       janpad: "Ramnagar",
-      policeStations: [
-        "Ramnagar",
-      ],
-    },
-  },
+      policeStations: ["Ramnagar"] // Correct
+    }
+  }
 };
-
 export default maihar;
--- data/location/satna.js
+++ data/location/satna.js
@@ -1,77 +1,47 @@
 const satna = {
   district: "Satna",
-
   tehsils: {
     "Raghuraj Nagar": {
       vidhansabha: "Satna",
-      janpad: "Satna",
-      policeStations: [
-        "Kotwali Satna",
-        "Civil Lines",
-        "Kolgawan",
-        "Jaitwara",
-      ],
+      janpad: "Sohawal", // Corrected from Satna
+      policeStations: ["Kotwali Satna", "Civil Lines", "Kolgawan"]
     },
-
     "Rampur Baghelan": {
       vidhansabha: "Rampur Baghelan",
       janpad: "Rampur Baghelan",
-      policeStations: [
-        "Rampur Baghelan",
-        "Nadan Dehat",
-      ],
+      policeStations: ["Rampur Baghelan"] 
     },
-
-    Nagod: {
+    "Nagod": {
       vidhansabha: "Nagod",
       janpad: "Nagod",
-      policeStations: [
-        "Nagod",
-        "Kothi",
-      ],
+      policeStations: ["Nagod", "Singhpur", "Jaso"] // Removed Kothi from here
     },
-
-    Unchehara: {
+    "Unchehara": {
       vidhansabha: "Nagod",
       janpad: "Unchehara",
-      policeStations: [
-        "Unchehara",
-      ],
+      policeStations: ["Unchehara"]
     },
-
-    Majhgawan: {
+    "Majhgawan": {
       vidhansabha: "Chitrakoot",
       janpad: "Majhgawan",
-      policeStations: [
-        "Majhgawan",
-        "Baroundha",
-      ],
+      policeStations: ["Majhgawan", "Baroundha", "Nayagaon", "Dharkundi"]
     },
-
-    Kotar: {
+    "Kotar": {
+      vidhansabha: "Raigaon", 
+      janpad: "Rampur Baghelan", // Corrected from Majhgawan
+      policeStations: ["Kotar"]
+    },
+    "Birsinghpur": {
       vidhansabha: "Raigaon",
-      janpad: "Majhgawan",
-      policeStations: [
-        "Kotar",
-      ],
+      janpad: "Sohawal", // Corrected from Majhgawan
+      policeStations: ["Jaitwara", "Sabhapur"] // Jaitwara/Sabhapur protect this zone
     },
-
-    Birsinghpur: {
-      vidhansabha: "Raigaon",
-      janpad: "Majhgawan",
-      policeStations: [
-        "Birsinghpur",
-      ],
-    },
-
-    Kothi: {
+    "Kothi": {
       vidhansabha: "Nagod",
       janpad: "Nagod",
-      policeStations: [
-        "Kothi",
-      ],
-    },
-  },
+      policeStations: ["Kothi"] // Maintained cleanly here
+    }
+  }
 };
 
 export default satna;
